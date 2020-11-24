d6b56dd81680b203f4543f1f223d5d7dc37d8865
Nassim <60872459+nassimk69@users.noreply.github.com>
2020-11-24 17:32:04 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-radio.onReceivedString(function on_received_string(receivedString: string) {
+radio.onReceivedString(function (receivedString) {
     ssd1306.clear()
     if (receivedString == "LT") {
         ssd1306.writeString("Brightness: ")
@@ -8,7 +8,6 @@ radio.onReceivedString(function on_received_string(receivedString: string) {
         ssd1306.writeNum(input.temperature())
         ssd1306.writeString(" degrees")
     }
-    
     if (receivedString == "TL") {
         ssd1306.writeString("Temperature: ")
         ssd1306.writeNum(input.temperature())
@@ -17,8 +16,12 @@ radio.onReceivedString(function on_received_string(receivedString: string) {
         ssd1306.writeString("Brightness: ")
         ssd1306.writeNum(input.lightLevel())
     }
-    
 })
-ssd1306.init(128, 64, DigitalPin.P0, 61)
+ssd1306.init(
+128,
+64,
+DigitalPin.P0,
+61
+)
 ssd1306.clear()
 radio.setGroup(1)
