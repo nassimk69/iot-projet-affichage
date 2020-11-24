d3c9d8193a100d4fd89b32995453b543d2b81a5b
Nassim <60872459+nassimk69@users.noreply.github.com>
2020-11-24 17:31:14 +0100

    Mise à jour pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+def on_received_string(receivedString):
+    ssd1306.clear()
+    if receivedString == "LT":
+        ssd1306.write_string("Brightness: ")
+        ssd1306.write_num(input.light_level())
+        ssd1306.new_line()
+        ssd1306.write_string("Temperature: ")
+        ssd1306.write_num(input.temperature())
+        ssd1306.write_string(" degrees")
+    if receivedString == "TL":
+        ssd1306.write_string("Temperature: ")
+        ssd1306.write_num(input.temperature())
+        ssd1306.write_string(" degrees")
+        ssd1306.new_line()
+        ssd1306.write_string("Brightness: ")
+        ssd1306.write_num(input.light_level())
+radio.on_received_string(on_received_string)
+
 ssd1306.init(128, 64, DigitalPin.P0, 61)
 ssd1306.clear()
-ssd1306.write_string("Temperature: ")
-ssd1306.write_num(input.temperature())
-ssd1306.write_string(" degrees")
-ssd1306.new_line()
-ssd1306.write_string("Brightness: ")
-ssd1306.write_num(input.light_level())
+radio.set_group(1)
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-radio.onReceivedString(function (receivedString) {
+radio.onReceivedString(function on_received_string(receivedString: string) {
     ssd1306.clear()
     if (receivedString == "LT") {
         ssd1306.writeString("Brightness: ")
@@ -8,6 +8,7 @@ radio.onReceivedString(function (receivedString) {
         ssd1306.writeNum(input.temperature())
         ssd1306.writeString(" degrees")
     }
+    
     if (receivedString == "TL") {
         ssd1306.writeString("Temperature: ")
         ssd1306.writeNum(input.temperature())
@@ -16,12 +17,8 @@ radio.onReceivedString(function (receivedString) {
         ssd1306.writeString("Brightness: ")
         ssd1306.writeNum(input.lightLevel())
     }
+    
 })
-ssd1306.init(
-128,
-64,
-DigitalPin.P0,
-61
-)
+ssd1306.init(128, 64, DigitalPin.P0, 61)
 ssd1306.clear()
 radio.setGroup(1)
